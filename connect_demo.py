7e47febf2ace22d7abeed6ab210e774aa51eb852
Mukilan Krishnakumar <mukilan.git@gmail.com>
2023-10-08 09:34:07 +0530

    Vector Search

--- connect_demo.py
+++ connect_demo.py
@@ -1,11 +1,22 @@
 import weaviate
 import json
+from api_keys import OPENAI_API_KEY
 
 auth_config = weaviate.AuthApiKey(api_key="readonly-demo")
 
 client = weaviate.Client(
         url= "https://edu-demo.weaviate.network",
-        auth_client_secret= auth_config
+        auth_client_secret= auth_config,
+        additional_headers={
+            "X-OpenAI-Api-Key": OPENAI_API_KEY
+            }
         )
-meta = client.get_meta()
-print(json.dumps(meta, indent=2))
+
+
+res = client.query.get(
+        "WikiCity", ["city_name", "country", "lng", "lat"]
+        ).with_near_text({
+            "concepts" : ["Major Indian city"]
+            }).with_limit(5).do()
+
+print(json.dumps(res, indent=2))
